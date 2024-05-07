fec961ca05e7568fa520a1f1db98f4d8130b2490
h-alice <admin@halice.art>
2024-05-07 17:24:32 +0800

    Move callback.

--- irc_client/irc_callback_commons.go
+++ irc_client/irc_callback_commons.go
@@ -1,1 +1,16 @@
 package irc_client
+
+import "time"
+
+func lastPongTracker(ircc *IrcClient, msg string) error {
+	parsed_message, err := ParseIrcMessage(msg)
+	if err != nil {
+		return err
+	}
+
+	if parsed_message.Command == "PONG" && parsed_message.Params[0] == "tmi.twitch.tv" {
+		ircc.lastPong = time.Now()
+	}
+
+	return nil
+}
