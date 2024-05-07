ac4128a0ccafdc98d0e93548f400ca16993d86e7
h-alice <admin@halice.art>
2024-05-07 18:47:51 +0800

    Add common IRC commands.

---
+++ irc_common_commands.go
@@ -0,0 +1,23 @@
+package irc
+
+func PRIVMSG(channel string, message string) IrcMessage {
+	return IrcMessage{
+		Command: "PRIVMSG",
+		Params:  []string{channel},
+		Message: message,
+	}
+}
+
+func JOIN(channel string) IrcMessage {
+	return IrcMessage{
+		Command: "JOIN",
+		Params:  []string{channel},
+	}
+}
+
+func PING(message string) IrcMessage {
+	return IrcMessage{
+		Command: "PING",
+		Message: message,
+	}
+}
