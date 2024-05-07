5db9a2c02e7949b9ced2800a2c4ea4909a3e2645
h-alice <admin@halice.art>
2024-05-07 15:15:52 +0800

    Message struct definition.

--- irc_client/irc_message.go
+++ irc_client/irc_message.go
@@ -1,1 +1,15 @@
 package irc_client
+
+type IrcMessagePrefix struct {
+	Nickname string
+	Username string
+	Hostname string
+}
+
+type IrcMessage struct {
+	Tags    map[string]string
+	Prefix  IrcMessagePrefix
+	Command string
+	Params  []string
+	Message string
+}
