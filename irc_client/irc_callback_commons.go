87cf55e8800773be0148abec248369e7fa065494
h-alice <admin@halice.art>
2024-05-07 18:01:16 +0800

    Ping pong handler.

--- irc_client/irc_callback_commons.go
+++ irc_client/irc_callback_commons.go
@@ -1,1 +1,17 @@
 package irc_client
+
+func pingHandler(ircc *IrcClient, msg string) error {
+	parsed_message, err := ParseIrcMessage(msg)
+	if err != nil {
+		return err
+	}
+
+	if parsed_message.Command == "PING" {
+		response := IrcMessage{
+			Command: "PONG",
+			Message: parsed_message.Message,
+		}
+		ircc.SendRawMessage(response.String())
+	}
+	return nil
+}
